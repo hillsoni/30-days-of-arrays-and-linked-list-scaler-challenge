93632d80adad5b97e441882ef065b9c38d37a194
Hill Soni <121160838+hillsoni@users.noreply.github.com>
2024-04-03 19:19:17 +0530

    Create day-03.cpp

---
+++ day-03.cpp
@@ -0,0 +1,10 @@
+int Solution::solve (vector<int> &A) {
+  int cnt = 0; 
+  int n A.size();
+  for(int i=0;i<A.size();i++){
+    if (A[i]%2!=0){
+      cnt++;
+      }
+  }
+  return min(cnt, n cnt);
+}
